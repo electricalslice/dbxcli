e45a10839b8b6c917d0e5656298d3c52f31c1982
Arafat Ali <arafatx@gmail.com>
2023-01-22 07:37:43 +0800

    Updated dbxcli and install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 # Run this script to install and you will be available to run this script as dbxcli
 
+echo -e "Removing previous dbxcli installation ...\n"
 rm -rf /usr/local/opencode/dbxcli
 
+echo -e "Creating a new dbxcli installation folder ...\n"
 mkdir -p /usr/local/opencode/dbxcli
 
+echo -e "Downloading dbxcli from github ...\n"
 git clone https://github.com/sofibox/dbxcli /usr/local/opencode/dbxcli
 
+echo -e "Giving execution permission to dropbox_uploader.sh and dbxcli ...\n"
 chmod +x /usr/local/opencode/dbxcli/dropbox_uploader.sh /usr/local/opencode/dbxcli/dbxcli
 
+echo -e "Removing previous dbxcli symbolic link ...\n"
 rm -f /usr/local/bin/dbxcli /usr/bin/dbxcli
 
-# Use command alias dbxcli
+echo -e "Creating a new dbxcli symbolic link ...\n"
 ln -s /usr/local/opencode/dbxcli/dbxcli /usr/local/bin
 
-# Run setup wizard
+echo -e "Running dbxcli reconfigure ...\n"
 dbxcli reconfigure
